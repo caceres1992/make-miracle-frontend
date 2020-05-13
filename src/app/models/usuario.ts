a3b050b5ad21c4d4780b7976a59368df1fd646f2
jasson <jassoncaceres98@gmail.com>
2020-05-13 18:31:41 -0500

    jasson comit

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,11 @@
+export class Usuario {
+
+    id: number;
+    nombre: string;
+    apellido: string;
+    username: string;
+    password: string;
+    enable: boolean;
+    roles: string[] = [];
+
+}
